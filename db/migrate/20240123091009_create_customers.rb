ecdfa85c38c447a6479b0aa8b29fd3a30afeba6b
ri0629 <tr08792@gmail.com>
2024-01-23 18:17:20 +0900

    db作成

--- db/migrate/20240123091009_create_customers.rb
+++ db/migrate/20240123091009_create_customers.rb
@@ -2,8 +2,8 @@ class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
       t.string :name
-      t.string :name
-      t.string :name
+      t.string :phone
+      t.string :type
 
 
 
