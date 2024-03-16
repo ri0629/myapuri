370c949635e04c17a1387e3ca330f95e9b2afbfd
ri0629 <tr08792@gmail.com>
2024-03-16 21:19:19 +0900

    createアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'customers#index'
   resources :customers, only: [:index, :new, :create, :edit, :update]
+  resources :customers do
+  resources :comments, only: :create
+end
 end
