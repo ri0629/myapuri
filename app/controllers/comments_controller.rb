959329461a4afcfa36e59c6321e7d0ac1a494a60
ri0629 <tr08792@gmail.com>
2024-03-19 00:13:08 +0900

    end 追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,7 @@ class CommentsController < ApplicationController
     Comment.create(comment_params)
 end
 
-
+private
+def comment_params
+  params.require(:comments).permit(:text).merge(user_id: current_user.id)
+end
