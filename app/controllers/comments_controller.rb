0fbe185094ef08acc76186139a1ce0fe641a6d16
ri0629 <tr08792@gmail.com>
2024-03-18 23:13:03 +0900

    commentコントローラーの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+end
+
+private
+def comment_params
+  params.require(:comments).permit(:text).merge(user_id: current_user.id)
